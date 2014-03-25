0577d89bd082edefa1c33d75a22f8f4e2e13e662
Matous Machacek <machacekmatous@gmail.com>
2014-03-25 22:54:08 +0100

    added models

---
+++ annotate/annotate/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from model_utils.managers import InheritanceManager
+
+class Project(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+
+    # For selecting subclasses
+    objects = InheritanceManager()
+
+    class Meta:
+        ordering = ['created']
+
+
--- annotate/ranks/models.py
+++ annotate/ranks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from model_utils.managers import InheritanceManager
+from annotate.models import Project
+from django.contrib.auth.models import User
 
-# Create your models here.
+class RankProject(Project):
+    instructions = models.TextField()
+
+class Item(models.Model):
+    project = models.ForeignKey(RankProject, related_name='items')
+    sentence_id = models.IntegerField()
+    source_sen = models.TextField()
+    source_seg = models.TextField()
+    candidate_segs = models.TextField()
+
+class Annotation(models.Model):
+    annotated_item = models.ForeignKey(Item, related_name='annotations')
+    ranks = models.TextField()
+    annotator = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now_add=True)    
