2e5754450c88f1377094ace994d17e34dabd876e
Matous Machacek <machacekmatous@gmail.com>
2014-04-03 01:39:31 +0200

    annotate

--- annotate/ranks/urls.py
+++ annotate/ranks/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from ranks.views import AnnotateView
+from ranks.views import ProjectListView, AnnotateView
 
 urlpatterns = patterns('',
-    url(r'^$', AnnotateView.as_view())
+    url(r'^projects/$', ProjectListView.as_view(), name="ranks.views.projectlistview"),
+    # url(r'^projects/(?P<pk>\d+)/$', ProjectDetailView.as_view(), name="s.views.projectdetailview"),
+    url(r'^projects/(?P<pk>\d+)/annotate/$', AnnotateView.as_view(), name="ranks.views.annotateview"),
 )
--- annotate/ranks/views.py
+++ annotate/ranks/views.py
@@ -1,8 +1,34 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, ListView
+from django.db.models import Count
+from ranks.models import RankProject, Segment
 #from django.shortcuts import render
 
 # Create your views here.
 
+class ProjectListView(ListView):
+    model = RankProject
+    template_name = "ranks/project_list.html"
+
 class AnnotateView(TemplateView):
     template_name = "ranks/annotate.html"
+    
+    def get_sentence(self):
+        sentence = Segment.objects\
+                .filter(sentence__project__pk = self.kwargs['pk'])\
+                .annotate(num_annot=Count('annotations'))\
+                .order_by('-num_annot')\
+                .first()\
+                .sentence
 
+        return sentence
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(AnnotateView, self).get_context_data(**kwargs)
+        sentence = self.get_sentence()
+        context['sentence'] = sentence
+
+        segment = sentence.segments.all().first()
+        context['candidates_str'] = segment.candidates_str.split('\n')
+
+
+        return context
