3f1168c75823caa961fa003b8f9c00daf3ddbb54
Matous Machacek <machacekmatous@gmail.com>
2014-05-07 19:58:38 +0200

    adding split from gize

---
+++ experiments/split-from-giza.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+from itertools import count
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Concatenate sentence alighned files to one big file for giza++", 
+        epilog="Author: Matous Machacek <machacekmatous@gmail.com>")
+
+    parser.add_argument("--change-prefix",
+            help="Change the directory of output files",
+            required=True,
+            dest="prefix",
+            )
+    
+    parser.add_argument("input",
+            help="Input file",
+            required=True,
+            type=argparse.FileType('r'),
+            dest="input")
+
+    parser.add_argument("--splitinfo",
+            help="A file with split information",
+            required=True,
+            type=argparse.FileType('r'),
+            dest="split_info")
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    input_iterator = iter(args.input)
+
+    for info_line in args.split_info: 
+
+        if output_file is None or n_lines is None:
+            raise ValueError("Number of output files does not match the number of records in split info")
+
+        n_lines, src_file, tgt_file = info_line.split('\t')
+        n_lines = int(n_lines)
+
+        path_list = tgt_file.split('/')
+        path_list[0] = args.prefix
+        dest_file_name = '/'.join(path_list)
+
+        with open(dest_file_name,'w') as output_file: 
+            for _ in xrange(n_lines):
+
+                try:
+                    line = next(input_iterator)
+                except StopIteration:
+                    raise ValueError("The input file is shorter than expected")
+
+            print(line.rstrip("\n"), file=output_file)
+
+    # Make sure, that the input iterator is empty
+    try:
+        next(input_iterator)
+        raise ValueError("The input file is longer than expected")
+    except StopIteration:
+        pass
+
+if __name__ == '__main__':
+    main()
