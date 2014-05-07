556ee6119ef4919f6bbb2ece2af41b186c9ca7ce
Matous Machacek <machacekmatous@gmail.com>
2014-05-07 18:20:47 +0200

    concat for giza implemented

---
+++ experiments/concat-for-giza.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+from itertools import izip, izip_longest, count
+import sys
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Concatenate sentence alighned files to one big file for giza++", 
+        epilog="Author: Matous Machacek <machacekmatous@gmail.com>")
+
+    parser.add_argument("--sources",
+            help="Source files",
+            required=True,
+            nargs='*',
+            type=argparse.FileType('r'),
+            )
+    
+    parser.add_argument("--targets",
+            help="Target files",
+            required=True,
+            nargs='*',
+            type=argparse.FileType('r'),
+            )
+    
+    parser.add_argument("out",
+            help="A file where source and target files will by concatenated, omit or '-' for stdout",
+            default=sys.stdout,
+            type=argparse.FileType('w'),
+            )
+
+    parser.add_argument("--splitinfo",
+            help="A file where target files will by concatenated",
+            required=True,
+            type=argparse.FileType('w'),
+            )
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    for source_file, target_file in izip_longest(args.sources, args.targets): 
+        for i, (source_line, target_line) in izip(count(1), izip_longest(source_file, target_file)):
+            print(source_line.strip(), target_line.strip(), file=args.out, sep='\t')
+        print(i, source_file.name, target_file.name, file=args.splitinfo, sep='\t')
+
+if __name__ == '__main__':
+    main()
