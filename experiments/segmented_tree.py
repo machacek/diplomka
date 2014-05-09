d35ada72a88afb4e91a5ebeee328f524981e8b36
Matous Machacek <machacekmatous@gmail.com>
2014-05-09 17:31:50 +0200

    printing original tokenized source sentence and segments

--- experiments/segmented_tree.py
+++ experiments/segmented_tree.py
@@ -36,7 +36,7 @@ def parse_args():
             help="A file with parsed source sentences",
             required=True,
             type=argparse.FileType('r'),
-            dest="parsed")
+            )
     
     parser.add_argument("--align",
             help="A file with source-target alignment",
@@ -48,13 +48,19 @@ def parse_args():
             help="A file with tokenized MT output",
             required=True,
             type=argparse.FileType('r'),
-            dest="target")
+            )
+    
+    parser.add_argument("--source",
+            help="A file with tokenized source",
+            required=True,
+            type=argparse.FileType('r'),
+            )
     
     parser.add_argument("--reference",
             help="A file with reference translation",
             required=True,
             type=argparse.FileType('r'),
-            dest="reference")
+            )
     
     parser.add_argument("--maxlength",
             help="Maximal length of a segment",
@@ -74,18 +80,19 @@ def parse_args():
 
 def main():
     args = parse_args()
-    for i, parse_str, ali_str, target_str, reference_str in izip(count(1), args.parsed, args.ali, args.target, args.reference):
+    for i, parse_str, ali_str, target_str, reference_str, source_str in izip(count(1), args.parsed, args.ali, args.target, args.reference, args.source):
         tree = SegmentedTree.convert(Tree(parse_str))
         target_tokenized = target_str.split()
-        source_tokenized = tree.leaves()
+        source_tokenized = source_str.split()
         reference_str = reference_str.strip()
         alignment = Alignment(ali_str)
         for segment, indexes in tree.segments_with_indexes(args.max_length):
             if len(segment) < args.min_length:
                 continue
+            source_segment = [source_tokenized[index] for index in indexes]
             mapped_indexes = alignment.range(indexes)
             mapped_segment = [target_tokenized[index] for index in mapped_indexes] 
-            print(i, " ".join(source_tokenized), reference_str, " ".join(segment), " ".join(mapped_segment), sep='\t')
+            print(i, " ".join(source_tokenized), reference_str, " ".join(source_segment), " ".join(mapped_segment), sep='\t')
 
 
 
