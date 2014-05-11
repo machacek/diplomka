86443038cbd8a07cc2cd6e31cbaa817d18c442ab
Matous Machacek <machacekmatous@gmail.com>
2014-05-12 00:28:06 +0200

    removing segranks to separate directory

--- segranks/segranks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from segranks.models import RankProject, Sentence, Segment, Annotation
-
-admin.site.register(RankProject)
-admin.site.register(Sentence)
-admin.site.register(Segment)
-admin.site.register(Annotation)
